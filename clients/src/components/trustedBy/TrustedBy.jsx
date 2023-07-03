d8440ac47b63b62eeaf3bd58581a20a6220848cf
Abhinav Tomar <ababhi7906@gmail.com>
2023-07-03 15:03:27 +0530

    initial commit

---
+++ clients/src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./TrustedBy.scss"
+const TrustedBy = () => {
+  return (
+    <div className='trustedBy'>
+      <div className='container'>
+        <span>Trusted By:</span>
+        <img src="./img/instagram.png" alt="" />
+        <img src="./img/facebook.png" alt="" />
+        <img src="./img/twitter.png" alt="" />
+        <img src="./img/linkedin.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default TrustedBy
